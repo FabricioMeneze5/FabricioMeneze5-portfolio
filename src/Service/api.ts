bafcfc63c0409cffe2e2a79ff085329717f0b2a4
FabricioMeneze5 <fabricio-menezes@live.com>
2025-08-22 09:29:08 +0100

    🧹 chore:  define tags technologies for each project

--- src/Service/api.ts
+++ src/Service/api.ts
@@ -16,67 +16,61 @@ export type Project = GitHubRepo & {
 const desiredRepos: { name: string; title: string; image: string; languages: string[] }[] = [
   {
     name: 'contact-list-95',
-    title: `Contact List 95'`,
+    title: `Contact List 95' OK`,
     image: 'https://picsum.photos/300?random=1',
-    languages: ['TypeScript', 'HTML'],
+    languages: ['Node', 'TypeScript', 'React', 'Redux', 'Styled-Components'],
   },
   {
     name: 'efood',
-    title: 'E Food',
+    title: 'E Food OK',
     image: 'https://picsum.photos/300?random=2',
-    languages: ['TypeScript', 'HTML'],
+    languages: ['Node', 'TypeScript', 'React', 'Redux', 'Styled-Components'],
   },
   {
     name: 'minhas-tarefas',
-    title: 'Minhas Tarefas',
+    title: 'Minhas Tarefas OK',
     image: 'https://picsum.photos/300?random=3',
-    languages: ['TypeScript', 'HTML'],
+    languages: ['Node', 'TypeScript', 'React', 'Redux', 'Styled-Components'],
   },
   {
     name: 'BMIcalculator',
-    title: 'BMI Calculator',
+    title: 'BMI Calculator OK',
     image: 'https://picsum.photos/300?random=4',
-    languages: ['JavaScript', 'HTML'],
+    languages: ['Node', 'TypeScript', 'React', 'Bootstrap'],
   },
   {
     name: 'my_birthday',
-    title: 'My Birthday',
+    title: 'My Birthday OK',
     image: 'https://picsum.photos/300?random=5',
-    languages: ['JavaScript', 'HTML', 'SCSS'],
-  },
-  {
-    name: 'OasisHotel2',
-    title: 'Oasis Hotel',
-    image: 'https://picsum.photos/300?random=6',
-    languages: ['JavaScript', 'HTML', 'SCSS'],
+    languages: ['JavaScript', 'HTML', 'Parcel', 'SASS'],
   },
   {
     name: 'clone_disneyplus',
-    title: 'Disney Plus clone',
+    title: 'Disney Plus clone OK',
     image: 'https://picsum.photos/300?random=7',
-    languages: ['JavaScript', 'HTML', 'SCSS'],
+    languages: ['JavaScript', 'HTML', 'SASS'],
   },
   {
     name: 'random_number_picker',
-    title: 'Random Number Picker',
+    title: 'Random Number Picker OK',
     image: 'https://picsum.photos/300?random=8',
-    languages: ['JavaScript', 'HTML', 'Less'],
+    languages: ['Node', 'JavaScript', 'HTML', 'Less'],
   },
   {
     name: 'jQuery-Photos-Gallery',
-    title: 'Photos Gallery',
+    title: 'Photos Gallery OK',
     image: 'https://picsum.photos/300?random=9',
-    languages: ['JavaScript', 'HTML', 'CSS'],
+    languages: ['jQuery', 'HTML', 'CSS'],
   },
   {
     name: 'agenda-de-contatos',
-    title: 'Agenda de Contatos',
+    title: 'Agenda de Contatos OK',
     image: 'https://picsum.photos/300?random=10',
     languages: ['JavaScript', 'HTML', 'CSS'],
   },
   {
     name: 'projeto_calculadora_medias',
-    title: 'Calculadora de Medias',
+    title: 'Calculadora de Medias OK',
     image: 'https://picsum.photos/300?random=11',
     languages: ['JavaScript', 'HTML', 'CSS'],
   },
@@ -92,11 +86,17 @@ const desiredRepos: { name: string; title: string; image: string; languages: str
     image: 'https://picsum.photos/300?random=12',
     languages: ['Java'],
   },
+  {
+    name: 'OasisHotel2',
+    title: 'Oasis Resort',
+    image: 'https://picsum.photos/300?random=13',
+    languages: ['JavaScript', 'HTML', 'SASS'],
+  },
 ];
 
 export const fetchProjects = async (): Promise<Project[]> => {
   try {
-    const repos = await fetch('https://api.github.com/users/FabricioMeneze5/repos', {
+    const repos = await fetch('https://api.github.com/users/FabricioMeneze5/subscriptions', {
       headers: { Authorization: `token ${GITHUB_TOKEN}` },
     });
     const reposJson: GitHubRepo[] = await repos.json();
