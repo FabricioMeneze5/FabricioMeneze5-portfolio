4ec405c2b64348319501a1d4183da79921000a23
FabricioMeneze5 <fabricio-menezes@live.com>
2025-08-22 08:32:53 +0100

    🎨 style: Tag colors for each technology

--- src/components/Tags/index.tsx
+++ src/components/Tags/index.tsx
@@ -1,5 +1,24 @@
 import { TagsContainer } from './styles';
 
+type Colors = {
+  [key: string]: string;
+};
+
+const colors: Colors = {
+  React: '#0A7EA4',
+  C: '#283593',
+  TypeScript: '#255b96',
+  JavaScript: '#C99A06',
+  Java: '#B07219',
+  CSS: '#264DE4',
+  SCSS: '#C6538C',
+  Redux: '#764ABC',
+  Bootstrap: '#563D7C',
+  HTML: '#D35400',
+  Node: '#3C873A',
+  default: '#444444',
+};
+
 type Props = {
   langKey: string;
   languages: string[];
@@ -8,7 +27,9 @@ type Props = {
 const Tags = ({ langKey, languages }: Props) => (
   <TagsContainer>
     {languages.map((lang) => (
-      <li key={`${langKey}-${lang}`}>{lang}</li>
+      <li key={`${langKey}-${lang}`} style={{ backgroundColor: colors[lang] || colors.default }}>
+        {lang}
+      </li>
     ))}
   </TagsContainer>
 );
